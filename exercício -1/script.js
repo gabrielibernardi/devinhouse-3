90ea87dd95bde66d97412b98bb2fc561759bf002
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2020-12-10 16:25:05 -0300

    exercício-1 - média dos valores atualizado

--- exercício -1/script.js
+++ exercício -1/script.js
@@ -1,4 +1,3 @@
-
 //média utilizando o forEach()
 
 const notas = [1, 5, 8, 3, 7, 10, 9, 4]
@@ -7,24 +6,30 @@ function calcularMediaDasNotas(notas) {
     let soma = 0
     let media
         notas.forEach(somaDosValores => {
-        soma = soma + somaDosValores 
+        soma += somaDosValores 
     }); 
     media = soma / notas.length
     return media
    }
 const result = calcularMediaDasNotas(notas)
-console.log(`A media dos valores é: ${result}`)
+console.log(`A media dos valores é: ${result}`)  
+
+// ou assim
+
+function calcularMedia(...numeros) {
+    let total = 0
+    numeros.forEach(numero => {
+        total = total + numero
+    })
+
+    media = total / numeros.length
+    console.log(media)
+}
+
+calcularMedia(5,8,5,10)
 
 
 
-/*média utilizando o reduce()
- const average = notas => notas.reduce((a, b) => a + b, 0) / notas.length
-const result = average(notas)
-
-console.log(`A media dos valores é: ${result}`) */
 
 
-
-
-
-
+ 
---
+++ script-aula.js
@@ -0,0 +1,35 @@
+
+
+
+
+/*média utilizando o reduce()
+ const average = notas => notas.reduce((a, b) => a + b, 0) / notas.length
+const result = average(notas)
+
+console.log(`A media dos valores é: ${result}`) */
+
+
+ const Funcionario = {
+    nome: "Gabrieli",
+    idade: "32",
+    cargo: "Desenvolvedora Web",
+    salario: "10.000",
+}
+
+const imprimirFuncionario = Funcionario => {
+const {nome, cargo, salario} = Funcionario
+console.log(`Nome: ${nome}, Cargo: ${cargo}, Salário: ${salario}`)   
+}
+imprimirFuncionario(Funcionario) 
+
+const Despesas = {
+    descrição: "Aluguel",
+    valor: "1300",
+}
+
+const valoresGastos = Despesas => {
+    const {valor} = Despesas
+    console.log(`O valor gasto com o aluguel é de ${valor}`)
+}
+
+valoresGastos(Despesas)
