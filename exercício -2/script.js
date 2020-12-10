516442f943850eaba3f82a5d21d898e9fa31cdf2
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2020-12-10 18:35:27 -0300

    exercício-2 -mapeando um array de números - resolvido

--- exercício -2/script.js
+++ exercício -2/script.js
@@ -3,9 +3,21 @@
 argumento para cada elemento do Array e devolve um novo 
 Array como resultado.*/
 
-const arrayValores = [1, 4, 9];
+// usando function
+
+/* const arrayValores = [1, 4, 9];
 
 const quadrados = arrayValores.map(function(numero) {
+  return numero * 2;
+});
+ console.log(quadrados)
+ console.log(arrayValores) */ 
+
+
+//usando arrow function
+ const arrayValores = [1, 4, 9];
+
+const quadrados = arrayValores.map(numero => {
   return numero * 2;
 });
  console.log(quadrados)
