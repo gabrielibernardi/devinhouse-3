2e6b4452c3d3b57154d5204ce47e0a23576b7aeb
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2020-12-11 08:44:57 -0300

    exercício-11-arrowfunction recebe objeto-resolvido

---
+++ exercício -11/script.js
@@ -0,0 +1,26 @@
+const Retangulo = {
+  altura: 30,
+  largura: 60,
+  }
+
+ 
+const calcularArea = Retangulo => {
+const {altura, largura} = Retangulo
+return altura * largura
+
+}
+console.log(calcularArea(Retangulo)) 
+
+ /* function calcularArea(altura, largura) {
+    let area = altura * largura
+    console.log(area)
+  }
+
+  calcularArea(30, 60) */
+
+
+  
+  
+
+
+
