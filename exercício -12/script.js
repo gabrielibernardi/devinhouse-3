bdbab74d2a66263b497ceca3287c690a1948a70d
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2020-12-11 08:58:41 -0300

    exercício-12-arrowfunction recebe parametro-resolvido

---
+++ exercício -12/script.js
@@ -0,0 +1,5 @@
+const impirmirDados = (cidade, bairro, estado) => {
+ return `Cidade ${cidade}, bairro ${bairro}, estado ${estado}`
+}
+
+console.log(impirmirDados("Florianópolis", "Carvoeira", "Santa Catarina"))
