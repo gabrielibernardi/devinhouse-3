43c06ab33b442abadcd02751c1faa7c5f3caca4b
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2020-12-10 22:10:56 -0300

    exercício-9-declarando arrow function-resolvido

---
+++ exercício -9/script.js
@@ -0,0 +1,15 @@
+/* function imprimirPessoa(nome, idade) {
+  return `Eu me chamo ${nome} e tenho ${idade} anos`
+}
+
+console.log(imprimirPessoa("gabrieli", 32)) */
+
+
+
+const imprimirPessoa = (nome, idade) => {
+  return `Eu me chamo ${nome} e tenho ${idade} anos`
+  
+}
+console.log(imprimirPessoa("gabrieli", 32))
+
+
